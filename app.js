e99b6daff05e48ad200fb3444edb716a3d78a51e
Anton Kulbitskiy <pa1nkller8879@gmail.com>
2022-10-21 19:46:07 +0300

    prettier fix

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
-async function getDashboardData(url = '/data.json') {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
+async function getDashboardData(url = "/data.json") {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
 
 class DashboardItem {
-    static PERIODS = {
-        daily: 'day',
-        weekly: 'week',
-        monthly: 'month',
-    }
+  static PERIODS = {
+    daily: "day",
+    weekly: "week",
+    monthly: "month",
+  };
 
-    constructor(data, container = '.dashboard__content', view = 'weekly') {
-        this.data = data;
-        this.container = document.querySelector(container);
-        this.view = view;
-        this._createMarkup();
-    }
+  constructor(data, container = ".dashboard__content", view = "weekly") {
+    this.data = data;
+    this.container = document.querySelector(container);
+    this.view = view;
+    this._createMarkup();
+  }
 
-    _createMarkup() {
-        const {title, timeframes} = this.data;
+  _createMarkup() {
+    const { title, timeframes } = this.data;
 
-        const id = title.toLowerCase().replace(/ /g, '-');
-        const {current, previous} = timeframes[this.view.toLowerCase()];
+    const id = title.toLowerCase().replace(/ /g, "-");
+    const { current, previous } = timeframes[this.view.toLowerCase()];
 
-        this.container.insertAdjacentHTML('beforeend', `
+    this.container.insertAdjacentHTML(
+      "beforeend",
+      `
             <div class="dashboard__item dashboard__item--${id}">
                 <article class="tracking-card">
                     <header class="tracking-card__header">
@@ -36,40 +38,49 @@ class DashboardItem {
                             ${current}hrs
                         </div>
                         <div class="tracking-card__prev-period">
-                            Last ${DashboardItem.PERIODS[this.view]} - ${previous}hrs
+                            Last ${
+                              DashboardItem.PERIODS[this.view]
+                            } - ${previous}hrs
                         </div>
                     </div>
                 </article>
             </div>
-        `);
+        `
+    );
 
-        this.time = this.container.querySelector(`.dashboard__item--${id} .tracking-card__time`);
-        this.prev = this.container.querySelector(`.dashboard__item--${id} .tracking-card__prev-period`);
-    }
-    changeView(view) {
-        this.view = view.toLowerCase();
-        const {current, previous} = this.data.timeframes[this.view.toLowerCase()];
+    this.time = this.container.querySelector(
+      `.dashboard__item--${id} .tracking-card__time`
+    );
+    this.prev = this.container.querySelector(
+      `.dashboard__item--${id} .tracking-card__prev-period`
+    );
+  }
+  changeView(view) {
+    this.view = view.toLowerCase();
+    const { current, previous } = this.data.timeframes[this.view.toLowerCase()];
 
-        this.time.innerText = `${current}hrs`;
-        this.prev.innerText = `Last ${DashboardItem.PERIODS[this.view]} - ${previous}hrs`;
-    }
+    this.time.innerText = `${current}hrs`;
+    this.prev.innerText = `Last ${
+      DashboardItem.PERIODS[this.view]
+    } - ${previous}hrs`;
+  }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    getDashboardData()
-        .then(data => {
-            const activities = data.map(activity => new DashboardItem(activity));
+document.addEventListener("DOMContentLoaded", () => {
+  getDashboardData().then((data) => {
+    const activities = data.map((activity) => new DashboardItem(activity));
 
-            const selectors = document.querySelectorAll('.view-selector__item');
-            selectors.forEach(selector =>
-                selector.addEventListener('click', function() {
-                    selectors.forEach(sel => sel.classList.remove('view-selector__item--active'))
-                    selector.classList.add('view-selector__item--active');
+    const selectors = document.querySelectorAll(".view-selector__item");
+    selectors.forEach((selector) =>
+      selector.addEventListener("click", function () {
+        selectors.forEach((sel) =>
+          sel.classList.remove("view-selector__item--active")
+        );
+        selector.classList.add("view-selector__item--active");
 
-                    const currentView = selector.innerText.trim().toLowerCase();
-                    activities.forEach(activity => activity.changeView(currentView));
-                })
-            )
-        })
-})
-
+        const currentView = selector.innerText.trim().toLowerCase();
+        activities.forEach((activity) => activity.changeView(currentView));
+      })
+    );
+  });
+});
